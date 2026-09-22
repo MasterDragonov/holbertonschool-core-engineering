e136e89575ad4cc20676e455d77a1d5c247ff0b8
MasterDragonov <emerson.keov@gmail.com>
2026-09-22 10:55:30 +1000

    new test

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+redft = 3.14
 print("Language: Python")
 print("Version: 3")
-print("Pi approx: 3.14")
-print("Computation valid: True")
+print(f"Pi approx: {redft}")
+if 5 < 10:
+	print("Computation valid: True")
